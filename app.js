7b5d2aa844796ba7f1b4843786e01d20040a0027
Thomas <thomasgeslin91@hotmail.fr>
2020-01-13 11:57:05 +0100

    Setting of the webpage, libraries and renderer window

---
+++ app.js
@@ -0,0 +1,18 @@
+
+//CREATING A SCENE
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); //(fov, aspect ratio, proximity field, background field )
+ 
+
+//CREATING A SPHERE
+const geometry = new THREE.SphereGeometry( 50, 32, 32 ); //radius, width segments, height segments
+const material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+const sphere = new THREE.Mesh( geometry, material );
+scene.add( sphere );
+
+
+// RENDERER
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight ); //aspect ratio renderer
+document.body.appendChild( renderer.domElement ); //add the renderer to the html 
