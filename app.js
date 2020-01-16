1856dd855ccd959fe56841e595d9e3c056fe0369
Thomas <thomasgeslin91@hotmail.fr>
2020-01-16 17:15:33 +0100

    Responsive resize of the window, view and renderer + disable Zoom

--- app.js
+++ app.js
@@ -2,13 +2,14 @@
 //CREATING A SCENE WITH CAMERA
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); //(fov, aspect ratio, proximity field, background field )
+const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 ); //(fov, aspect ratio, proximity field, background field )
 
 
 //CREATING A SPHERE + TEXTURE
 const geometry = new THREE.SphereGeometry( 50, 32, 32 ); //radius, width segments, height segments
 const textureLoader = new THREE.TextureLoader()
 const texture = textureLoader.load("360.png")
+texture.minFilter = THREE.LinearFilter;  //block the resizing of the image
 const material = new THREE.MeshBasicMaterial( {
 	map:texture,
 	side:THREE.DoubleSide
@@ -27,6 +28,8 @@ document.body.appendChild( renderer.domElement ); //add the renderer to the html
 
 const controls = new THREE.OrbitControls( camera, renderer.domElement );
 camera.position.set( 1, 0, 0 ) //camera centered
+controls.rotateSpeed = 0.2
+controls.enableZoom = false
 controls.update()
 
 //UPDATING CAMERA RENDER
@@ -38,3 +41,12 @@ function animate() {
 
 }
 animate()
+
+function onResize(){
+	renderer.setSize(window.innerWidth, innerHeight)
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
+	
+}
+
+window.addEventListener('resize', onResize)
