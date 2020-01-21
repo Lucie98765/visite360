890d2e0660444e3980c1fb934c099b1008140444
Unknown <lucie.lesbats@hotmail.fr>
2020-01-21 10:54:46 +0100

    Js admin access

--- display.js
+++ display.js
@@ -3,5 +3,13 @@ document.addEventListener("DOMContentLoaded", initialiser);
 
 
 function initialiser(evt) {
-    
+    var lesInputs = document.getElementById("photos").querySelectorAll("input");
+    for (var unInput of lesInputs){
+        unInput.addEventListener("click", selectionner_photo); 
+    }
+}
+
+
+function selectionner_photo(){
+    this.previousElementSibling.classList.toggle('photoSelec'); 
 }
