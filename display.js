7c5048a2c7f11e2f1830332162c684911102a6b7
Unknown <lucie.lesbats@hotmail.fr>
2020-01-22 10:14:36 +0100

    onglets retractables

--- display.js
+++ display.js
@@ -7,9 +7,18 @@ function initialiser(evt) {
     for (var unInput of lesInputs){
         unInput.addEventListener("click", selectionner_photo); 
     }
+    
+    var lesOnglets = document.querySelectorAll("h2");
+    for (var unOnglet of lesOnglets){
+        unOnglet.addEventListener("click", plier_deplier); 
+    }
 }
 
 
 function selectionner_photo(){
     this.previousElementSibling.classList.toggle('photoSelec'); 
+}
+
+function plier_deplier(){
+    this.nextElementSibling.classList.toggle('replier');
 }
