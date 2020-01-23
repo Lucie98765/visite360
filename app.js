09ce89306d3db56a71a120e4b9e0d0928f1434ba
Thomas <thomasgeslin91@hotmail.fr>
2020-01-23 11:44:43 +0100

    v1. Working version (black sprites, no css)

--- app.js
+++ app.js
@@ -1,46 +1,112 @@
-
-
 const container = document.body
 const tooltip = document.querySelector('.tooltip')
 let spriteActive = false
 
-//CREATING A SCENE WITH CAMERA
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 ); //(fov, aspect ratio, proximity field, background field )
+class Scene{
 
-
-//CREATING A SPHERE + TEXTURE
-const geometry = new THREE.SphereGeometry( 50, 32, 32 ); //radius, width segments, height segments
-const texture = new THREE.TextureLoader().load("360.png")
-
-texture.wrapS = THREE.RepeatWrapping
-texture.repeat.x = -1 //reverse picture 
-texture.generateMipmaps = false;
-texture.minFilter = THREE.LinearFilter;
-const material = new THREE.MeshBasicMaterial( {
-	map:texture,
-	side:THREE.DoubleSide
-} );
-const sphere = new THREE.Mesh( geometry, material );
-scene.add( sphere );
-
-
-
-//TOOLTIP
-function addTooltip (position, name){
+	constructor(image, points){
+		this.image = image
+		this.points = []
+		this.sprites = []
+		this.scene = null
+	}
 	
-	let spriteMap = new THREE.TextureLoader().load( "sprite.png" );
-	let spriteMaterial = new THREE.SpriteMaterial( { 
-	 	map: spriteMap
-		} );
-	let sprite = new THREE.Sprite( spriteMaterial )
-	sprite.name = name
-	sprite.position.copy(position.clone().normalize().multiplyScalar(30))
-	sprite.scale.multiplyScalar(2)
-	scene.add( sprite )
+	createScene(scene){
+		this.scene = scene
+		const geometry = new THREE.SphereGeometry( 50, 32, 32 ); //radius, width segments, height segments
+		const texture = new THREE.TextureLoader().load(this.image)
+		texture.wrapS = THREE.RepeatWrapping
+		texture.repeat.x = -1 //reverse picture 
+		const material = new THREE.MeshBasicMaterial( {
+			map:texture,
+			side:THREE.DoubleSide
+		} )
+		material.transparent = true
+		this.sphere = new THREE.Mesh(geometry,material)
+		this.scene.add(this.sphere)
+		this.points.forEach(this.addTooltip.bind(this))
+	}
+
+	addPoint (point){
+		this.points.push(point) 
+	}
+
+	addTooltip (point){
+		let spriteMap = new THREE.TextureLoader().load( "sprite.png" );
+		let spriteMaterial = new THREE.SpriteMaterial( { 
+		 	map: spriteMap
+			} );
+		let sprite = new THREE.Sprite( spriteMaterial )
+		sprite.name = point.name
+		sprite.position.copy(point.position.clone().normalize().multiplyScalar(30))
+		sprite.scale.multiplyScalar(2)
+		this.scene.add(sprite)
+		this.sprites.push(sprite)
+		sprite.onClick = () => {
+			this.destroy()
+			point.scene.createScene(scene)
+			point.scene.appear()
+		}
+	}
+
+	destroy(){
+		gsap.to(this.sphere.material, 1, {
+			opacity: 0,
+			onComplete: () => {
+				this.scene.remove(this.sphere)
+			}
+		})
+		this.sprites.forEach((sprite) => {
+				gsap.to(sprite.scale, 1, {
+					x: 0,
+					y: 0,
+					z: 0,
+					onComplete: () => {
+						this.scene.remove(sprite)
+					}
+				})
+		})
+	}
+
+	appear(){
+		this.sphere.material.opacity = 0
+		gsap.to(this.sphere.material, 1, {
+			opacity: 1
+		})
+		this.sprites.forEach((sprite) => {
+			sprite.scale.set(0,0,0)
+			gsap.to(sprite.scale, 1, {
+				x: 2,
+				y: 2,
+				z: 2
+			})
+		})
+	}
 
 }
+//CREATING A SCENE WITH CAMERA
+
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 ) //(fov, aspect ratio, proximity field, background field )
+
+//CREATING A SPHERE 
+let s = new Scene('360.png', [])
+let s2 = new Scene('3602.png', [])
+
+s.addPoint({
+	position: new THREE.Vector3(41.66006320223875, 1.7799572824780827,-27.43192409158089),
+	name: "Copernic",
+	scene: s2
+})
+
+s2.addPoint({
+	position: new THREE.Vector3(1,0,0.5),
+	name: "Sortie",
+	scene: s
+})
+s.createScene(scene)
 
 // RENDERER
 const renderer = new THREE.WebGLRenderer();
@@ -82,8 +148,8 @@ function onClick(e) {
 	rayCaster.setFromCamera(mouse, camera)
 	let intersects = rayCaster.intersectObjects(scene.children)
 	intersects.forEach(function (intersect){
-		if (intersect.object.type == 'Sprite'){
-			console.log(intersect.object.name)
+		if (intersect.object.type === 'Sprite'){
+			intersect.object.onClick()
 		}
 	})
 /*	let intersects = rayCaster.intersectObject(sphere)
@@ -110,19 +176,15 @@ function onMouseMove (e) {
 			tooltip.innerHTML = intersect.object.name
 			spriteActive = intersect.object
 			foundSprite = true
-			gsap.to(intersect.object.scale,  .3, {x: 3, y:3, z:3 } );
 		}
 	})
 	if (foundSprite === false && spriteActive){
 		tooltip.classList.remove('is-active')
-		gsap.to(spriteActive.scale,  .3, {x: 2, y:2, z:2 } )
-
-	spriteActive = false
+		spriteActive = false
 		
 	}
 }
 
-addTooltip(new THREE.Vector3(41.66006320223875, 1.7799572824780827,-27.43192409158089), "Copernic")
 
 animate()
 window.addEventListener('resize', onResize)
